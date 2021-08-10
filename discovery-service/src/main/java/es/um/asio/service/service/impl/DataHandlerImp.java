db584116f1edd44e19ab8c5c38b1a0fbd6d472a1
Daniel Ruiz Santamaria <druiz@izertis.com>
2021-08-10 11:17:26 +0200

    change to load real data

--- discovery-service/src/main/java/es/um/asio/service/service/impl/DataHandlerImp.java
+++ discovery-service/src/main/java/es/um/asio/service/service/impl/DataHandlerImp.java
@@ -135,7 +135,7 @@ public class DataHandlerImp implements DataHandler {
             updateState(DataType.CACHE,cache.getTriplesMap());
         }
         // Update data from triple store (add deltas)
-        if(false)
+        if(true)
             updateCachedData(); //  quit comment
         applicationState.setDataState(DataType.CACHE, State.UPLOAD_DATA);
         // Update elasticSearch
