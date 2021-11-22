aff27d2a6e98706001537a973c42d3414cacac92
Daniel Ruiz Santamaria <druiz@izertis.com>
2021-11-22 13:34:46 +0100

    improve logs inverser dependencies 2

--- discovery-service/src/main/java/es/um/asio/service/service/impl/CacheServiceImp.java
+++ discovery-service/src/main/java/es/um/asio/service/service/impl/CacheServiceImp.java
@@ -413,6 +413,9 @@ public class CacheServiceImp implements CacheService {
         if (attValue!=null && Utils.isValidString(attValue.toString())) {
             // uc  => Destino
             URIComponent uc = Utils.getInstanceLink(attValue.toString(), serviceImp.getCanonicalSchema(), domain);
+            logger.info("Domain", domain);
+            logger.info("Canonical Schema", serviceImp.getCanonicalSchema());
+            logger.info("URI", attValue.toString());
             logger.info(uc.toString());
             if (uc != null && uc.getConcept() != null && uc.getReference() != null) {
                 TripleObject toTarget = getTripleObject(toOrigin.getTripleStore().getNode().getNodeName(), toOrigin.getTripleStore().getName(), uc.getConcept(), uc.getReference());
